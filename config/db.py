e07d60c54f8e135039f7bbc90622c2fc3582ead0
Lizet Lopez <lizet.chlopez@gmail.com>
2025-09-29 09:44:51 -0600

    Deleted test mongo uri

--- config/db.py
+++ config/db.py
@@ -7,14 +7,9 @@ load_dotenv()
 
 def get_db():
 
-
     uri = os.getenv("MONGO_URI")
     db_name = os.getenv("DB_NAME")
-   
-    '''
-    uri ="mongodb+srv://admin:admin@bancodetiempobackend.k3kc8om.mongodb.net/banco_tiempo_db?retryWrites=true&w=majority&appName=bancodetiempobackend"
-    db_name ="banco_tiempo_db"
-     '''
+
     print("Uri cargada:", uri)
 
     try:
